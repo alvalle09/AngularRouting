8849166b108624cd20dccf681b6affb1be6f9afe
alvalle09 <phredvalle@gmail.com>
2020-02-27 23:44:54 -0600

    formatting and documentation added

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -17,6 +17,7 @@ export class ServerComponent implements OnInit {
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+    
     this.route.params
       .subscribe(
         (params: Params) => {
