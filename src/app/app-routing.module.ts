ee99e58a326c93023a6bda1a6b891a18e2b60649
alvalle09 <phredvalle@gmail.com>
2020-03-02 23:18:21 -0600

    use canActivate and route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ServersComponent } from "./servers/servers.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
     // Home Component
@@ -16,7 +17,7 @@ const appRoutes: Routes = [
       { path: ':id/:name', component: UserComponent }
     ] },
     // Servers component routes
-    { path: 'servers', component: ServersComponent, children: [
+    { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent }
     ] },
